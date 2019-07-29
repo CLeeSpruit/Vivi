c3e13e010b0e50ffdd494285c992945b54123b16
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2019-07-29 17:51:38 -0500

    Added publish script

---
+++ publish.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+stage="./dist"
+
+echo "Is this a major, minor, or patch update?"
+select update in "major" "minor" "patch";
+do
+    npm version $update
+    break
+done
+
+echo "Building and staging files..."
+npm run dist
+
+# Copy over newly edited package.json
+cp package.json $stage
+
+cd $stage
+npm publish --scope=public
+cd ../
+
+echo "Publish complete!"
+echo "Push changes to github?"
+select push in "Yes" "No"; 
+do
+    case $push in
+        Yes) git push
+        break
+        ;;
+        No) echo "Skipping push. Don't forget to do it yourself!"
+        break
+        ;;
+    esac
+done
